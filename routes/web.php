5a307e777d2b28b8ebe6fcf6358709ed938be511
Tanuththiga <anuththiga25jt@gmail.com>
2023-02-08 13:57:16 +0530

    custom register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(UserController::class)->group(function(){
+    Route::get('login', 'index')->name('login');
+    Route::get('registration', 'registration')->name('registration');
+    Route::get('logout', 'logout')->name('logout');
+    Route::post('validate_registration', 'validate_registration')->name('user.validate_registration');
 });
